612f8591b8a3e917956cbad049eef156701f68ae
halifox <106858061+halifox@users.noreply.github.com>
2024-11-12 12:40:53 +0800

    Add exports for additional marker and driving functionalities in amap.

--- lib/amap.dart
+++ lib/amap.dart
@@ -3,6 +3,8 @@
 /// injection, and route management in a quick and practical way.
 library amap;
 
+export 'package:js/js.dart';
+
 export 'js/amap.dart';
 export 'js/base/Bounds.dart';
 export 'js/base/LngLat.dart';
@@ -14,13 +16,17 @@ export 'js/control/HawkEye.dart';
 export 'js/control/MapType.dart';
 export 'js/control/Scale.dart';
 export 'js/control/ToolBar.dart';
+export 'js/driving/Driving.dart';
 export 'js/ext.dart';
 export 'js/layer/TileLayer.dart';
 export 'js/loader.dart';
 export 'js/location/Geolocation.dart';
+export 'js/marker/ElasticMarker.dart';
+export 'js/marker/IconMarker.dart';
+export 'js/marker/LabelMarker.dart';
+export 'js/marker/Marker.dart';
+export 'js/marker/TextMarker.dart';
 export 'js/search/AutoComplete.dart';
 export 'js/search/CloudDataSearch.dart';
 export 'js/search/PlaceSearch.dart';
-export 'js/driving/Driving.dart';
 export 'view/amap_view.dart';
-export 'package:js/js.dart';
